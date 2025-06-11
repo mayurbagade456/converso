83c82fc4845f6c310c5f13367c0407ee0893d118
mayurbagade456 <mayurbagade456@gmail.com>
2025-06-11 12:46:53 +0530

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  typescript: {
+      ignoreBuildErrors: true
+    },
+    eslint: {
+        ignoreDuringBuilds: true
+    },
+  images: {
+      remotePatterns: [
+          { hostname: 'img.clerk.com'}
+      ]
+  }
   /* config options here */
 };
 
